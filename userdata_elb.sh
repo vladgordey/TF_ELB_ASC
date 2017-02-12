cd83099499512fff14ee200f23db0ac7da63948e
vladgordey <vladgordey@gmail.com>
2017-02-12 16:47:47 -0500

    Update userdata_elb.sh

--- userdata_elb.sh
+++ userdata_elb.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -v
-sudo su
-apt-get update -y
-apt-get install apache2
-apt-get install php5
-apt-get install libapache2-mod-php5
-/etc/init.d/apache2 restart
-cd /var/www/
-echo '<? php phpinfo() ?>' >> mytest.php
+sudo yum update
+sudo yum -y install httpd
+cd /var/www/html/
+echo '<Hey BODY! How are you doing?>' >> index.html
+sudo service httpd start
 
