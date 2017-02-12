16cb1b50574a1553ba8a75bdfee72db7a6c8bf33
vladgordey <vladgordey@gmail.com>
2017-02-12 11:56:04 -0500

    Update main_elb.tf

--- main_elb.tf
+++ main_elb.tf
@@ -50,7 +50,7 @@ resource "aws_autoscaling_group" "app" {
   min_size             = "${var.asg_min}"
   max_size             = "${var.asg_max}"
   desired_capacity     = "${var.asg_desired}"
-  launch_configuration = "${aws_launch_configuration.app.name}"
+  launch_configuration = "${aws_launch_configuration.gordey_lc.name}"
 }
 
 resource "aws_launch_configuration" "gordey_lc" {
