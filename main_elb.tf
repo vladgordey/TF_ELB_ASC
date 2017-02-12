8f1adab46bd88f99341aadbf75e57efed047461f
vladgordey <vladgordey@gmail.com>
2017-02-12 11:31:52 -0500

    Add files via upload

---
+++ main_elb.tf
@@ -0,0 +1,206 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+data "aws_availability_zones" "available" {}
+
+
+###=================================================== VPC ========================================###
+# New VPC for env
+resource "aws_vpc" "main" {
+  cidr_block = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  tags {
+    Name = "${var.short_name}-vpc"
+  }
+}
+resource "aws_subnet" "main" {
+  count             = "${var.az_count}"
+  cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  vpc_id            = "${aws_vpc.main.id}"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = "${var.az_count}"
+  subnet_id      = "${element(aws_subnet.main.*.id, count.index)}"
+  route_table_id = "${aws_route_table.r.id}"
+}
+
+
+###=================================================== ASG ========================================###
+
+resource "aws_autoscaling_group" "app" {
+  name                 = "vlad-gordey-asg"
+  vpc_zone_identifier  = ["${aws_subnet.main.*.id}"]
+  min_size             = "${var.asg_min}"
+  max_size             = "${var.asg_max}"
+  desired_capacity     = "${var.asg_desired}"
+  launch_configuration = "${aws_launch_configuration.app.name}"
+}
+
+resource "aws_launch_configuration" "gordey_lc" {
+  name          = "gordey-lc"
+  image_id      = "ami-5b31fd34"
+  instance_type = "${var.instance_type}"
+
+  # Security group
+  security_groups = ["${aws_security_group.elb.id}"]
+}
+
+###=================================================== ELB ========================================###
+
+resource "aws_elb" "web" {
+  name = "vgordey-elb"
+
+  # The same availability zone as our instance
+  availability_zones = ["${aws_instance.web.availability_zone}"]
+  security_groups    = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  # The instance is registered automatically
+  instances = ["${aws_instance.web.id}"]
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
+resource "aws_lb_cookie_stickiness_policy" "default" {
+  name                     = "lbpolicy"
+  load_balancer            = "${aws_elb.web.id}"
+  lb_port                  = 80
+  cookie_expiration_period = 600
+}
+
+resource "aws_instance" "web" {
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  # The name of our SSH keypair you've created and downloaded
+  # from the AWS console.
+  #
+  # https://console.aws.amazon.com/ec2/v2/home?region=us-west-2#KeyPairs:
+  #
+  key_name = "${var.key_name}"
+
+  # Our Security group to allow HTTP and SSH access
+  security_groups = ["${aws_security_group.default.name}"]
+
+  user_data = "${file("userdata_elb.sh")}"
+
+  #Instance tags
+  tags {
+    Name = "Vgordey_elb"
+  }
+}
+
+
+###=================================================== Secirity Groups ========================================###
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "instance_sg"
+  description = "Used in the terraform"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Our elb security group to access
+# the ELB over HTTP
+resource "aws_security_group" "elb" {
+  vpc_id = "${aws_vpc.main.id}"
+  name   = "vladgordey-ecs-lbsg"
+  description = "Used in the terraform"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # HTTPS access from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+      cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+}
+
+
+
---
+++ variables_elb.tf
@@ -0,0 +1,42 @@
+variable "key_name" {
+  description = "Name of the SSH keypair to use in AWS."
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "eu-central-1"
+}
+
+# ubuntu-trusty-14.04 (x64)
+variable "aws_amis" {
+  default = {
+    "eu-central-1" = "ami-5b31fd34"
+    "us-west-2" = "ami-7f675e4f"
+  }
+}
+
+variable "short_name" {default = "gordey"}
+
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "AWS instance type"
+}
+variable "asg_min" {
+  description = "Min numbers of servers in ASG"
+  default     = "1"
+}
+
+variable "asg_max" {
+  description = "Max numbers of servers in ASG"
+  default     = "2"
+}
+
+variable "asg_desired" {
+  description = "Desired numbers of servers in ASG"
+  default     = "1"
+}
