815eed477ba3bcf9dc3b60fd296644e4a30c9aad
vladgordey <vladgordey@gmail.com>
2017-02-12 16:06:22 -0500

    Update userdata_elb.sh

--- userdata_elb.sh
+++ userdata_elb.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -v
+sudo su
 apt-get update -y
-apt-get install -y nginx > /tmp/nginx.log
+apt-get install apache2
+apt-get install php5
+apt-get install libapache2-mod-php5
+/etc/init.d/apache2 restart
+cd /var/www/
+echo '<? php phpinfo() ?>' >> mytest.php
 
