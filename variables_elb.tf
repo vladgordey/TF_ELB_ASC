cb996d9a2e554974dcde3113b1306f6bf757a9ad
vladgordey <vladgordey@gmail.com>
2017-02-12 16:13:38 -0500

    Update variables_elb.tf

--- variables_elb.tf
+++ variables_elb.tf
@@ -29,12 +29,12 @@ variable "instance_type" {
 }
 variable "asg_min" {
   description = "Min numbers of servers in ASG"
-  default     = "1"
+  default     = "2"
 }
 
 variable "asg_max" {
   description = "Max numbers of servers in ASG"
-  default     = "2"
+  default     = "4"
 }
 
 variable "asg_desired" {
