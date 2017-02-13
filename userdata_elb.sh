16fd7e402b879cddb2ca8c30d9f0bcf9fa6661ba
vladgordey <vladgordey@gmail.com>
2017-02-13 13:52:13 -0500

    Update userdata_elb.sh

--- userdata_elb.sh
+++ userdata_elb.sh
@@ -1,6 +1,12 @@
-#!/bin/bash -v
-sudo yum update
-sudo yum -y install httpd
-cd /var/www/html/
-echo '<Hey BODY! How are you doing?>' >> index.html
-sudo systemctl start httpd.service
+#!/bin/bash
+yum update -y
+yum install -y httpd24 php56 mysql55-server php56-mysqlnd
+service httpd start
+chkconfig httpd on
+groupadd www
+usermod -a -G www ec2-user
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} +
+find /var/www -type f -exec chmod 0664 {} +
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
